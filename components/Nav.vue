eea9a7632dc97fd7068299837c1ceaf1921d004b
JonSelby Co <jonathan.selby@nike.com>
2021-07-15 18:38:39 -0700

    (feat) Navigation responsive styling

--- components/Nav.vue
+++ components/Nav.vue
@@ -5,13 +5,21 @@
       <span>Parents of Students in Special Education</span>
     </div>
 
-    <ol>
-      <li>About</li>
-      <li>Services</li>
-      <li>Tutoring</li>
-      <li>FAQ</li>
-      <li>Contact Us</li>
-    </ol>
+    <div>
+      <ol>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/sevices">Services</NuxtLink></li>
+        <li><NuxtLink to="/tutoring">Tutoring</NuxtLink></li>
+        <li><NuxtLink to="/faq">FAQ</NuxtLink></li>
+        <li><NuxtLink to="/contact">Contact Us</NuxtLink></li>
+      </ol>
+
+      <div class="nav-mobile">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   </nav>
 </template>
 
@@ -22,13 +30,31 @@
     justify-content: space-between;
     flex-wrap: wrap;
     padding: 10px;
+    color: #566050;
+
+    a {
+      color: #566050;
+      font-size: 20px;
+      transition: 0.5s ease-in-out;
+      text-decoration: none;
+
+      &:hover {
+        color: #bcddac;
+      }
+    }
 
     ol {
       display: flex;
       list-style: none;
+      margin: 10px 0px;
+
+      @media only screen and (max-width: 820px) {
+        display: none;
+      }
 
       li {
-        padding: 0 8px;
+        font-size: 18px;
+        padding: 0 16px;
       }
     }
   }
@@ -37,11 +63,41 @@
     flex-direction: column;
     span {
       &:first-child {
-        font-size: 30px;
+        font-size: 36px;
+        font-weight: bold;
       }
       &:last-child {
         font-size: 16px;
+        color: #111;
       }
+    }
+  }
+
+  &-mobile {
+    display: none;
+    border: 1px solid #849c7a;
+    padding: 10px;
+    border-radius: 30%;
+    transition: 0.5s ease-in-out;
+
+    &:hover {
+      background: #849c7a;
+      cursor: pointer;
+    }
+    div {
+      border: 1px solid #d5b9b2;
+      border-radius: 20%;
+      background: #d5b9b2;
+      width: 20px;
+      height: 2px;
+      margin-bottom: 4px;
+
+      &:last-child {
+        margin-bottom: 0px !important;
+      }
+    }
+    @media only screen and (max-width: 820px) {
+      display: block;
     }
   }
 }
---
+++ pages/tutoring.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Tutoring Page</div>
+</template>
